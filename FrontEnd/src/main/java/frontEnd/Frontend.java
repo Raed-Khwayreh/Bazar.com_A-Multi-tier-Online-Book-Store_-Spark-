6840b21c295a480ba2704f5f2a1d50e961bccea5
Raed-Khwayreh <raedkh.2000@gmail.com>
2022-11-08 20:39:51 +0200

    update

--- FrontEnd/src/main/java/frontEnd/Frontend.java
+++ FrontEnd/src/main/java/frontEnd/Frontend.java
@@ -10,34 +10,34 @@ public class Frontend {
 		String category;
 	}
 	public static void main(String[] args) {	
-		port(7777);
+		port(9878);
 		get("/info/:id", (request, response) -> {
-			response.redirect("http://192.168.1.3:5555/info/"+request.params(":id"));
+			response.redirect("http://192.168.1.14:5555/info/"+request.params(":id"));
 			
 			return null;
 		});
 		post("/info", (request, response) -> {
 			Book bookID = new Gson().fromJson(request.body(), Book.class);
-			response.redirect("http://192.168.1.3:5555/info/"+bookID.id +"");
+			response.redirect("http://192.168.1.14:5555/info/"+bookID.id +"");
 			return null;
 		});
 		get("/search/:cateName", (request, response) -> {
-			response.redirect("http://192.168.1.3:5555/search/"+request.params(":cateName"));
+			response.redirect("http://192.168.1.14:5555/search/"+request.params(":cateName"));
 
 			return null;
 		});
 		post("/search", (request, response) -> {
 			Book bookCategory = new Gson().fromJson(request.body(), Book.class);
-			response.redirect("http://192.168.1.3:5555/search/"+bookCategory.category +"");
+			response.redirect("http://192.168.1.14:5555/search/"+bookCategory.category +"");
 			return null;
 		});
-		put("purchase/:id", (request, response) -> {
-			response.redirect("http://192.168.1.3:9999/purchase/"+request.params(":id"));
+		post("purchase/:id", (request, response) -> {
+			response.redirect("http://192.168.1.14:9999/purchase/"+request.params(":id"));
 			return null;
 		});
-		put("/purchase", (request, response) -> {
+		post("/purchase", (request, response) -> {
 			Book bookID = new Gson().fromJson(request.body(), Book.class);
-			response.redirect("http://192.168.1.3:9999/purchase/"+bookID.id +"");
+			response.redirect("http://192.168.1.14:9999/purchase/"+bookID.id +"");
 			return null;
 		});
 		
--- OrderServer/src/main/java/orderServer/OrderServer.java
+++ OrderServer/src/main/java/orderServer/OrderServer.java
@@ -1,12 +1,12 @@
 package orderServer;
-
+ 
 import static spark.Spark.*;
 
 public class OrderServer {
 	public static void main(String[] args) {
 		port(9999);
 		get("/purchase/:id", (request, response) -> {
-			response.redirect("http://192.168.1.3:5555/infoToPurchase/"+request.params(":id"));
+			response.redirect("http://192.168.1.14:5555/infoToPurchase/"+request.params(":id"));
 			return null;
 		});
 	}
